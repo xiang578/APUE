feed815f361cbbbe343dc49595fda28f5fbb04f6
xiang578 <xiang578@foxmail.com>
2017-09-29 15:49:39 +0800

    01 done

---
+++ 01_UNIX_System_Overview/printpID.c
@@ -0,0 +1,7 @@
+#include "apue.h"
+
+int main()
+{
+    printf("This process ID: %ld\n",(long)getpid());
+    exit(0);
+}
---
+++ 01_UNIX_System_Overview/printuidgid.c
@@ -0,0 +1,7 @@
+#include "apue.h"
+
+int main()
+{
+    printf("uid = %d, pid = %d\n", getuid(), getpid());
+    exit(0);
+}
